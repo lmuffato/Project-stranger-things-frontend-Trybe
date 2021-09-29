7bd9dafaac62a583b72f9a7c1671bfe3ec8ed93d
Rafael Gerônimo <rafael@geronimo.dev>
2021-09-29 19:00:39 -0300

    fix: commit dotenv file

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -15,12 +15,12 @@ const getRealityClass = (hereIsTheUpsideDownWorld) => (
 
 const strangerThingsConfig = {
   url: REACT_APP_HAWKINS_URL,
-  timeout: REACT_APP_HAWKINS_TIMEOUT,
+  timeout: Number(REACT_APP_HAWKINS_TIMEOUT),
 };
 
 const upsideDownConfig = {
   url: REACT_APP_UPSIDEDOWN_URL,
-  timeout: REACT_APP_UPSIDEDOWN_TIMEOUT,
+  timeout: Number(REACT_APP_UPSIDEDOWN_TIMEOUT),
 };
 
 const charactersService = new CharactersService(strangerThingsConfig);
