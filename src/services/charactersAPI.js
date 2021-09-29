b11e48795bedd471da3f2fbeaeecf67bcedd73fe
maricegalla <maricegalla@gmail.com>
2021-09-29 18:49:00 -0300

    Requirement 1

--- src/services/charactersAPI.js
+++ src/services/charactersAPI.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+require('dotenv').config();
+
 const DEFAULT_TIMEOUT = 30000;
 class CharactersService {
   constructor({ url = 'http://localhost:3000', timeout = DEFAULT_TIMEOUT }) {
