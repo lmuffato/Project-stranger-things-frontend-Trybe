18b9a9c6c1dd95bc43ef4edabfdc4e5f139ad99b
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-09-29 20:26:49 -0300

    [new] Modo desenvolvedor

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 import StrangerThings from './components/StrangerThings';
 
 function App() {
+  const isDeveloperMode = JSON.parse(process.env.REACT_APP_DEVELOPER_MODE);
   return (
     <div className="App">
+      { isDeveloperMode && <div>Em desenvolvimento</div> }
       <StrangerThings />
     </div>
   );
