320c7370df5b1cf314943307fbc9fbce31f16075
Viegas <mvrdgs@gmail.com>
2021-09-29 20:46:55 -0300

    ajuste nome variavel de ambiente

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -12,7 +12,7 @@ const strangerThingsConfig = {
 };
 
 const upsideDownConfig = {
-  url: process.env.REACT_APP_UPSIDEDOWN,
+  url: process.env.REACT_APP_UPSIDEDOWN_URL,
   timeout: process.env.REACT_APP_UPSIDEDOWN_TIMEOUT,
 };
 
