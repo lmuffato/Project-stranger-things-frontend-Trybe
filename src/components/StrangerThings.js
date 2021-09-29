dcc7f672c5f14e9a3fa60b9217a120d5583fd45f
joaoAndradeJr <fullservicemanaus@gmail.com>
2021-09-29 17:23:07 -0400

    change import of the dotenv lib

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import CharactersService from '../services/charactersAPI';
 import Table from './Table';
+require('dotenv').config();
 
 const {
   REACT_APP_HAWKINS_URL,
   REACT_APP_HAWKINS_TIMEOUT,
   REACT_APP_UPSIDEDOWN_URL,
   REACT_APP_UPSIDEDOWN_TIMEOUT,
-} = window.env;
+} = process.env;
 
 const getRealityClass = (hereIsTheUpsideDownWorld) => (
   hereIsTheUpsideDownWorld ? 'upside-down' : 'stranger-things'
--- src/services/charactersAPI.js
+++ src/services/charactersAPI.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+const DEFAULT_TIMEOUT = 30000;
 class CharactersService {
-  constructor({ url, timeout }) {
+  constructor({ url = 'http://localhost:3000', timeout = DEFAULT_TIMEOUT }) {
     this.http = axios.create({
       baseURL: url,
       timeout,
