0c7f186afffa48c5af76c41718bf3d4614907ff5
Stanrley Barbosa <3011stan@gmail.com>
2021-09-29 16:21:48 -0300

    [3011stan] Inicio de projeto

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 import StrangerThings from './components/StrangerThings';
-// change
+// Change
 function App() {
   return (
     <div className="App">
