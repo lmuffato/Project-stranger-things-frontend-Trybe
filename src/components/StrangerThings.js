37059394c058adc37393a8fa24e669d3e041cf10
Jodiel Briesemeister <jodielll29@gmail.com>
2021-09-29 20:24:46 -0300

    feat(req09): heroku add msg

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -129,6 +129,7 @@ class StrangerThings extends React.Component {
               Mudar de Realidade
             </button>
           </div>
+          <p>Em desenvolvimento</p>
           <div>
             <input
               placeholder="Nome do Personagem"
