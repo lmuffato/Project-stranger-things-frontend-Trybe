574f5cc4b95502b40037e24fd344b1df86105200
giovanni-maldonado <giovannitadeu08@gmail.com>
2021-09-29 21:00:08 -0300

    heroku uploaded

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -6,7 +6,7 @@ const getRealityClass = (hereIsTheUpsideDownWorld) => (
   hereIsTheUpsideDownWorld ? 'upside-down' : 'stranger-things'
 );
 
-const strangerThingsConfig = {
+const strangerThingsConfig = { // teste
   url: process.env.REACT_APP_HAWKINS_URL,
   timeout: process.env.REACT_APP_HAWKINS_TIMEOUT,
 };
