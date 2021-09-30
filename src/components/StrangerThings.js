7f4a4c4bd2b7ce06b9bb0cc00eaee3aec4675956
Anderson Nascimento <anderson.nascimentoafsn@gmail.com>
2021-09-30 15:16:38 -0300

    feat: development tag

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -145,6 +145,7 @@ class StrangerThings extends React.Component {
             <button type="button" onClick={ this.nextPage }>Próximo</button>
           </div>
         </div>
+        <p>Em desenvolvimento</p>
       </div>
     );
   }
