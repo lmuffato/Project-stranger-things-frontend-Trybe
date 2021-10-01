fe08fb8efe295a209c92e9aa0a7f8c0a77cba0ed
Michael <michaelpetterson06.com.br>
2021-10-01 08:52:45 -0300

    criando ambiente de desenvolvimento

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import StrangerThings from './components/StrangerThings';
 function App() {
   return (
     <div className="App">
+      <div>Em desenvolvimento</div>
       <StrangerThings />
     </div>
   );
