3994e3de1b9c1c9f4269fb401330dee5b20e837d
Michael <michaelpetterson06.com.br>
2021-10-01 09:36:02 -0300

    ambiente pd

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import StrangerThings from './components/StrangerThings';
 function App() {
   return (
     <div className="App">
-      <div>Em desenvolvimento</div>
       <StrangerThings />
     </div>
   );
