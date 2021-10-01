c477df4a58bf71e97e49daac8858b63bebd36347
Hudson Carlos <hc88806@gmail.com>
2021-10-01 14:16:42 -0400

    add pd

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import StrangerThings from './components/StrangerThings';
 function App() {
   return (
     <div className="App">
-      <header>Em desenvolvimento</header>
       <StrangerThings />
     </div>
   );
