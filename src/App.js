ab247e3de3cb2a91c47add35b10105ced2fcf804
defauth98 <neto.daniribeiro@gmail.com>
2021-10-01 06:17:21 -0300

    1 - .env

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 import StrangerThings from './components/StrangerThings';
 
 function App() {
+  const devMode = process.env.REACT_APP_DEV_MODE === 'true';
   return (
     <div className="App">
+      {devMode && <h2>Em desenvolvimento</h2>}
       <StrangerThings />
     </div>
   );
