dc9f3abb78c644978789928b63e6b5e0244a60c6
RenzoSev <sevilharenzo@gmail.com>
2021-09-30 21:03:08 -0300

    feat: show dev mode

--- src/App.js
+++ src/App.js
@@ -4,8 +4,14 @@ import './App.css';
 import StrangerThings from './components/StrangerThings';
 
 function App() {
+  const isDevMode = process.env.REACT_APP_DEV_MODE === 'true';
+
+  const renderElementDevMode = () => <h2>Em desenvolvimento</h2>;
+
   return (
     <div className="App">
+      {isDevMode && renderElementDevMode()}
+
       <StrangerThings />
     </div>
   );
