746a330824488c461b60cbb4521ac91db78c661b
Gui-lira <gui.lira.martins@gmail.com>
2021-10-02 19:06:36 -0300

    requisito 9 pre heroku

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -7,12 +7,12 @@ const getRealityClass = (hereIsTheUpsideDownWorld) => (
 );
 
 const strangerThingsConfig = {
-  url: 'http://localhost:3002',
+  url: process.env.HAWKINS,
   timeout: 30000,
 };
 
 const upsideDownConfig = {
-  url: 'http://localhost:3003',
+  url: process.env.UPSIDEDOWN,
   timeout: 30000,
 };
 
