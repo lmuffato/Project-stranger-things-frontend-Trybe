7c1a0551e6366b8e812891805a1e40fdc84533f4
Parker9309 <juan.tentiomaru9309@gmail.com>
2021-10-02 02:10:10 -0300

    fix: req8 test

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-// só comentando para ter coisa para commitar
+// só comentando para ter coisa para commitar 2
 
 ReactDOM.render(
   <React.StrictMode>
