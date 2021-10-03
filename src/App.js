ea01d48a9aa9f0e7d23bf695032821bbdbc6090c
negres <nataliaknegres@gmail.com>
2021-10-03 11:28:26 -0300

    Adiciona requisitos

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import './App.css';
 
 import StrangerThings from './components/StrangerThings';
 
+const ENVIROMENT_DEV = process.env.REACT_APP_ENVIROMENT_DEV === 'true';
+const renderDevEnvironmentMessage = () => <h1>Em desenvolvimento</h1>;
+
 function App() {
   return (
     <div className="App">
+      { ENVIROMENT_DEV && renderDevEnvironmentMessage() }
       <StrangerThings />
     </div>
   );
