188db2ce9aabc8ead67273c0be2a6731bc1a2d5a
Rodrigo Rech <rodrigorech92@gmail.com>
2021-10-04 18:54:18 -0300

    Feat: em deselvivomento deleted

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -115,7 +115,6 @@ class StrangerThings extends React.Component {
           hereIsTheUpsideDownWorld,
         )}` }
       >
-        <h2>Em desenvolvimento</h2>
         <div className="content strangerfy">
           <div className="change-reality">
             <button type="button" onClick={ this.changeRealityClick }>
