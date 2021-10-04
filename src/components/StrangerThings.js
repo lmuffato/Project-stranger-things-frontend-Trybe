ce28a4db327af294f5693404703004d9fec63d7c
Maria Baeta <m.martinsdg@gmail.com>
2021-10-03 22:53:36 -0300

    feat: :sparkles: Requisito 8

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -120,6 +120,9 @@ class StrangerThings extends React.Component {
         )}` }
       >
         <div className="content strangerfy">
+          {
+            process.env.MODE === true && <p>Em desenvolvimento</p>
+          }
           <div className="change-reality">
             <button type="button" onClick={ this.changeRealityClick }>
               {' '}
