25862660b7a96101c0b54cb2be440071baad087c
Guilherme Dugaich de Oliveira <guidugaich.dev@gmail.com>
2021-10-05 14:57:06 -0300

    using NODE_ENV for environment vars

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import './App.css';
 
 import StrangerThings from './components/StrangerThings';
 
+require('dotenv').config();
+
 function App() {
+  const mode = process.env.NODE_ENV;
+
   return (
     <div className="App">
+      {mode === 'development' && 'Em desenvolvimento'}
       <StrangerThings />
     </div>
   );
