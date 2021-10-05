b9f5efdec55a5241bf0b41096ea44ea33eeae5b5
Gustavo Sousa <gustavohl98@gmail.com>
2021-10-05 17:57:54 -0300

    Initial commit. Iniciando o projeto #VQV :rocket:

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -7,12 +7,12 @@ const getRealityClass = (hereIsTheUpsideDownWorld) => (
 );
 
 const strangerThingsConfig = {
-  url: 'http://localhost:3002',
+  url: 'http://localhost:3000',
   timeout: 30000,
 };
 
 const upsideDownConfig = {
-  url: 'http://localhost:3003',
+  url: 'http://localhost:3000',
   timeout: 30000,
 };
 
