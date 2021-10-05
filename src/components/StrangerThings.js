f5a8eec83e439e77c5bf9e7bba9587b171bbdb7f
igson <igson660@gmail.com>
2021-10-04 21:04:39 -0500

    configura variaveis heroku

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -2,6 +2,7 @@ import React from 'react';
 import CharactersService from '../services/charactersAPI';
 import Table from './Table';
 
+// alteração feita
 require('dotenv').config();
 
 const getRealityClass = (hereIsTheUpsideDownWorld) => (
