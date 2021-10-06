a5f6aa66333e7ea1b924032019c73c6511a5f7c8
luizfelipe-dev <thelastnailmusic@gmail.com>
2021-10-06 15:35:30 -0300

    deployed

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import './App.css';
 
 import StrangerThings from './components/StrangerThings';
 
+require('dotenv').config();
+
 function App() {
+  console.log(process.env);
   return (
     <div className="App">
+      { process.env.REACT_APP_STAGING === 'true' && 'Em desenvolvimento' }
       <StrangerThings />
     </div>
   );
--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -2,18 +2,20 @@ import React from 'react';
 import CharactersService from '../services/charactersAPI';
 import Table from './Table';
 
+require('dotenv').config();
+
 const getRealityClass = (hereIsTheUpsideDownWorld) => (
   hereIsTheUpsideDownWorld === 'true' ? 'upside-down' : 'stranger-things'
 );
 
 const strangerThingsConfig = {
-  url: 'https://luizfelipe-dev-bk.herokuapp.com/',
-  timeout: 30000,
+  url: process.env.REACT_APP_HAWKINS_URL,
+  timeout: process.env.REACT_APP_HAWKINS_TIMEOUT,
 };
 
 const upsideDownConfig = {
-  url: 'https://luizfelipe-dev-bd.herokuapp.com/',
-  timeout: 30000,
+  url: process.env.REACT_APP_UPSIDEDOWN_URL,
+  timeout: process.env.REACT_APP_UPSIDEDOWN_TIMEOUT,
 };
 
 const charactersService = new CharactersService(strangerThingsConfig);
