50f0e0fb6b554614d946f7facbcd9b37e8571788
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-10-07 16:01:27 -0300

    1st requirement completed

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -6,14 +6,20 @@ const getRealityClass = (hereIsTheUpsideDownWorld) => (
   hereIsTheUpsideDownWorld ? 'upside-down' : 'stranger-things'
 );
 
+const REACT_APP_HAWKINS_URL = 'http://localhost:3002';
+const REACT_APP_HAWKINS_TIMEOUT = 30000;
+
 const strangerThingsConfig = {
-  url: 'http://localhost:3002',
-  timeout: 30000,
+  url: REACT_APP_HAWKINS_URL,
+  timeout: REACT_APP_HAWKINS_TIMEOUT,
 };
 
+const REACT_APP_UPSIDEDOWN_URL = 'http://localhost:3003';
+const REACT_APP_UPSIDEDOWN_TIMEOUT = 30000;
+
 const upsideDownConfig = {
-  url: 'http://localhost:3003',
-  timeout: 30000,
+  url: REACT_APP_UPSIDEDOWN_URL,
+  timeout: REACT_APP_UPSIDEDOWN_TIMEOUT,
 };
 
 const charactersService = new CharactersService(strangerThingsConfig);
