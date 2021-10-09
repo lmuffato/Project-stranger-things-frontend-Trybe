07e76615697ea7a1b45dc9443c0896de848f1a45
lmuffato <lmiremuffato@gmail.com>
2021-10-09 01:25:23 -0300

    fix requisito 3

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import StrangerThings from './components/StrangerThings';
 function App() {
   return (
     <div className="App">
-      { process.env.REACT_APP_DEV_MODE ? (<div>Em desenvolvimento</div>) : null }
+      {/* { process.env.REACT_APP_DEV_MODE ? (<div>Em desenvolvimento</div>) : null } */}
       <StrangerThings />
     </div>
   );
