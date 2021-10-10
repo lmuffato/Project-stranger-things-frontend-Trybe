6a2f29cea1b17913544dde6b88f013baf89eeee0
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-10-10 18:48:45 -0300

    development mode

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -115,6 +115,7 @@ class StrangerThings extends React.Component {
           hereIsTheUpsideDownWorld,
         )}` }
       >
+        <div><p>Em desenvolvimento</p></div>
         <div className="content strangerfy">
           <div className="change-reality">
             <button type="button" onClick={ this.changeRealityClick }>
