bd77b0c5a5b4b913d65acd30ad7c1afe923719c1
Marina Alane <marina.alane@outlook.com>
2021-10-11 14:50:38 -0300

    deploy development

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -120,7 +120,7 @@ class StrangerThings extends React.Component {
               Mudar de Realidade
             </button>
           </div>
-
+          <p>Em desenvolvimento</p>
           <div>
             <input
               placeholder="Nome do Personagem"
