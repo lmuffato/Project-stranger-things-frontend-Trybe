061e2c35dab3d489ef2fb83d7cd4755028ae538c
Britosl1 <lucasbrito.carneiro@gmail.com>
2021-10-13 18:21:32 -0300

    Requirement8

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -2,24 +2,19 @@ import React from 'react';
 import CharactersService from '../services/charactersAPI';
 import Table from './Table';
 
-require('dotenv').config();
-
-const { REACT_APP_HAWKINS_URL,
-  REACT_APP_HAWKINS_TIMEOUT,
-  REACT_APP_UPSIDEDOWN_URL,
-  REACT_APP_UPSIDEDOWN_TIMEOUT } = process.env;
+require('dotenv/config');
 
 const getRealityClass = (hereIsTheUpsideDownWorld) => (hereIsTheUpsideDownWorld
   ? 'upside-down' : 'stranger-things');
 
 const strangerThingsConfig = {
-  url: REACT_APP_HAWKINS_URL,
-  timeout: REACT_APP_HAWKINS_TIMEOUT,
+  url: process.env.REACT_APP_HAWKINS_URL,
+  timeout: process.env.REACT_APP_HAWKINS_TIMEOUT,
 };
 
 const upsideDownConfig = {
-  url: REACT_APP_UPSIDEDOWN_URL,
-  timeout: REACT_APP_UPSIDEDOWN_TIMEOUT,
+  url: process.env.REACT_APP_UPSIDEDOWN_URL,
+  timeout: process.env.REACT_APP_UPSIDEDOWN_TIMEOUT,
 };
 
 const charactersService = new CharactersService(strangerThingsConfig);
