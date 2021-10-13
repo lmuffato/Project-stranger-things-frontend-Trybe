4ac9421d59f6e932184a0bbcd13033de301d1309
Britosl1 <lucasbrito.carneiro@gmail.com>
2021-10-13 18:53:26 -0300

    Add variables

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import './App.css';
 
 import StrangerThings from './components/StrangerThings';
 
+require('dotenv').config();
+
+const devVar = !process.env.ENVIROMENT_VAR;
+const flag = () => <h1>Em desenvolvimento</h1>;
+
 function App() {
   return (
     <div className="App">
+      { devVar && flag() }
       <StrangerThings />
     </div>
   );
