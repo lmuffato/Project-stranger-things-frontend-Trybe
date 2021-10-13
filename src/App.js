efa079c09cfeaae168a0c6e7f614186f802cb08d
Southier <patrick.southier@hotmail.com>
2021-10-12 23:50:19 -0300

    feat: create a environment var to see the In Development flag on App when on the development remote

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
 
+require('dotenv').config();
+
+const appInDev = process.env.DEV_ENVIRONMENT === 'true';
+const inDevFlag = () => {
+  <h1>Em desenvolvimento</h1>
+};
+
 import StrangerThings from './components/StrangerThings';
 
 function App() {
   return (
     <div className="App">
+      { appInDev && inDevFlag() }
       <StrangerThings />
     </div>
   );
